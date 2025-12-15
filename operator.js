9bebcbd65b3a59f06a7ad947f0c9010e38b772ed
abrarbalia <baliaabrar@gmail.com>
2025-12-15 10:33:10 +0530

    update the new ternary operator

--- operator.js
+++ operator.js
@@ -59,3 +59,7 @@ console.log("value of a after pre increment is :",a);
 let age=21;
 let resut=(age>20) ?"eligible":"not eligible";
 console.log("The person is :",resut);   
+
+let marks=90;
+let result=(marks>=33)?"pass":"fail";
+console.log("The student is :",result);
--- tempCodeRunnerFile.js
+++ tempCodeRunnerFile.js
@@ -1,22 +1,3 @@
-
-let student=[{
-    name:"abrar",
-    age:21,
-    cgpa:3.75
-},
-{
-    name:"sabbir",
-    age:22,
-    cgpa:3.85
-},
-{
-    name:"jamal",
-    age:23,
-    cgpa:3.95
-}];
-let key=Object.keys(student[0]);
-console.log(key);
-student.forEach(stud=> {
-console.log(`${stud.name}|${stud.age}|${stud.cgpa}`);
-// console.log(stud.age,stud.name,stud.cgpa);
-});
+let marks=90;
+let result=(marks>=33)?"pass":"fail";
+console.log("The student is :",result);
